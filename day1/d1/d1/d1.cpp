1fa4fd0676fbffae9f37892c098fe966ef407686
MedeeaCorinaG <131515356+MedeeaCorinaG@users.noreply.github.com>
2025-05-06 11:37:05 +0300

    ziua 1

--- day1/d1/d1/d1.cpp
+++ day1/d1/d1/d1.cpp
@@ -1,6 +1,18 @@
 
 #include <iostream>
 using namespace std;
+int suma(int a, int b) {
+    return a + b;
+}
+int diferenta(int a, int b) {
+    return a - b;
+}
+int produs(int a, int b) {
+    return a * b;
+}
+int impartire(int a, int b) {
+    return a / b;
+}
 int main()
 {/* afisare a 5 numere
     int n = 5;
@@ -8,7 +20,7 @@ int main()
         cout << i << endl;
 
     }
-    */
+    
     int a = 0;
     cout << "Valoare i++: " << a++ << endl;
     cout << "Dupa i++: " << a << endl;
@@ -16,6 +28,12 @@ int main()
     int b = 0;
     cout << "Valoare ++i: " << ++b << endl;
     cout << "Dupa ++i: " << b << endl;
+    */
+    
+    cout << suma(2, 3);
+    cout << diferenta(4, 5);
+    cout << produs(2, 3);
+    cout << impartire(6, 3);
+
 
 }
-
