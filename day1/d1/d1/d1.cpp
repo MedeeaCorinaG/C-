91f6e43ce2f4c1ce78e5bc454585551c8c8f864f
MedeeaCorinaG <131515356+MedeeaCorinaG@users.noreply.github.com>
2025-05-06 11:29:22 +0300

    ziua1

--- day1/d1/d1/d1.cpp
+++ day1/d1/d1/d1.cpp
@@ -1,8 +1,21 @@
 
 #include <iostream>
-
+using namespace std;
 int main()
-{
-    std::cout << "Hello World!\n";
+{/* afisare a 5 numere
+    int n = 5;
+    for (int i = 0; i <= n; i++) {
+        cout << i << endl;
+
+    }
+    */
+    int a = 0;
+    cout << "Valoare i++: " << a++ << endl;
+    cout << "Dupa i++: " << a << endl;
+
+    int b = 0;
+    cout << "Valoare ++i: " << ++b << endl;
+    cout << "Dupa ++i: " << b << endl;
+
 }
 
