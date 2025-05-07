50a623202e6593a2d733a0b147dbc0eef362baa7
MedeeaCorinaG <131515356+MedeeaCorinaG@users.noreply.github.com>
2025-05-07 18:19:04 +0300

    ziua 2

--- day2/d2/d2/d2.cpp
+++ day2/d2/d2/d2.cpp
@@ -5,7 +5,22 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+	/*int a = 10;
+	int* p = &a;
+	cout << "val lui a:" << a<<endl;
+	cout << "adresa lui a:" << &a << endl;
+	cout << "val lui p:" << p << endl;
+	cout << "val lui p:" << *p << endl;
+	
+
+	int a = 5;
+	int* p = &a;
+	*p = 20;
+	cout << "Valoarea lui a: " << a << endl;
+	cout << "Valoarea prin pointer: " << *p << endl;
+	*/
+
+
 }
 
 
