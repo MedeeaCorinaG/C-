8d373c6d8db0df3def90b9f1cf17b89b34b8e83a
MedeeaCorinaG <131515356+MedeeaCorinaG@users.noreply.github.com>
2025-05-08 12:02:30 +0300

    ziua 2

--- day2/d2/d2/d2.cpp
+++ day2/d2/d2/d2.cpp
@@ -8,6 +8,19 @@ void swap(int* a, int* b) {
 	*b = temp;
 
 }
+void afiseaza_vector(int* v, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "Valoare: " << *(v + i) << " | Adresa: " << (v + i) << endl;
+	}
+}
+int suma_vector(int* v, int n) {
+	int S = 0;
+	for (int i = 0; i < n; i++) {
+		S += *(v + i);
+		
+	}
+	return S;
+}
 int main()
 {
 	/*int a = 10;
@@ -23,13 +36,20 @@ int main()
 	*p = 20;
 	cout << "Valoarea lui a: " << a << endl;
 	cout << "Valoarea prin pointer: " << *p << endl;
-	*/
+	
 	int x = 5, y = 10;
 
 	cout << "Inainte de swap: x = " << x << ", y = " << y << endl;
 	swap(&x, &y);
 	cout << "Dupa swap: x = " << x << ", y = " << y << endl;
-
+	
+	int v[5] = { 2,6,9,4,1 };
+	for (int i = 0; i < 5; i++) {
+		cout << v[i] << " - " << &v[i] << endl;
+	}*/
+	int v[5] = { 5,2,9,5,4 };
+	int total = suma_vector(v, 5);
+	cout << "Suma elementelor: " << total << endl;
 
 }
 
